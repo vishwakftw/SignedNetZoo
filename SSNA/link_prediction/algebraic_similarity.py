9fd69d0a2b1bcd5ed1fa041b4d7002e931097a41
vishwakftw <cs15btech11043@iith.ac.in>
2018-11-08 19:07:52 +0530

    Add setup.py, exponential prediction

--- SSNA/link_prediction/algebraic_similarity.py
+++ SSNA/link_prediction/algebraic_similarity.py
@@ -13,7 +13,6 @@ def adjacency_dim_reduce(G, dim, required_links):
     Args:
         G : Graph to consider as training data
         dim : Dimensionality to reduce the adjacency matrix to.
-              If dim > rank of adjacency matrix, then error out.
         required_links : List of tuples (a, b) where (a, b) denotes the outgoing
                          edge from `a` to `b`.
 
@@ -44,7 +43,6 @@ def symmetric_adjacency_dim_reduce(G, dim, required_links):
     Args:
         G : Graph to consider as training data
         dim : Dimensionality to reduce the adjacency matrix to.
-              If dim > rank of adjacency matrix, then error out.
         required_links : List of tuples (a, b) where (a, b) denotes the outgoing
                          edge from `a` to `b`.
 
@@ -58,6 +56,43 @@ def symmetric_adjacency_dim_reduce(G, dim, required_links):
     for pair in required_links:
         i, j = pair
         entry_val = sum([w[k] * v[i, k] * v[j, k] for k in range(dim)])
+        if entry_val >= 0:
+            preds.append(1)
+        else:
+            preds.append(-1)
+    return preds
+
+
+def exponential_adjacency_dim_reduce(G, dim, required_links, symmetric=False):
+    """
+    Function to get predictions for the parity of a link via dimensionality
+    reduction techniques on the exponential of the (symmetric) adjacency matrix.
+    The link between u and v is predicted as follows:
+        EA = exp(A) => EA = U S V^{T} => EA_{dim} = U_{dim} S_{dim} V^{T}_{dim}.
+    link(u, v) = sign(EA_{dim}[u, v])
+
+    Args:
+        G : Graph to consider as training data
+        dim : Dimensionality to reduce the exponential of the adjacency to.
+        required_links : List of tuples (a, b) where (a, b) denotes the outgoing
+                         edge from `a` to `b`.
+        symmetric : If ``True`` then the exponential of the symmetric adjacency matrix
+                    is considered. Else, the ordinary adjacency matrix is taken.
+
+    Returns:
+        List of {+1, -1} based on the properties of the graph
+    """
+    if symmetric:
+        A = get_symmetric_adjacency_matrix(G)
+    else:
+        A = get_adjacency_matrix(G)
+    A = A.astype(float)
+    EA = sspl.expm(A)
+    u, s, v = sspl.svds(EA, k=dim)
+    preds = []
+    for pair in required_links:
+        i, j = pair
+        entry_val = sum([s[k] * u[i, k] * v[k, j] for k in range(dim)])
         if entry_val >= 0:
             preds.append(1)
         else:
---
+++ setup.py
@@ -0,0 +1,23 @@
+import os
+from setuptools import setup, find_packages
+
+requirements = [
+    'networkx',
+    'numpy',
+    'scipy',
+]
+
+VERSION = '0.0.1'
+
+setup(
+    name='SSNA',
+    version=VERSION,
+    url='https://github.com/vishwakftw/CS6270-TDBMS',
+    description='Datasets, tools and prediction algorithms for signed social networks',
+    license='MIT',
+
+    packages=find_packages(exclude=('test',)),
+
+    zip_safe=True,
+    install_requires=requirements,
+)
